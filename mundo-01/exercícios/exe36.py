de47121c96b4e897aaaeac41a1e52ee323b1627d
Danilo Soares <danilo.ssoares20@gmail.com>
2023-06-14 17:04:41 -0300

    Update exe36.py

--- mundo-01/exercícios/exe36.py
+++ mundo-01/exercícios/exe36.py
@@ -1,4 +1,5 @@
-n ="danilo"
+n ="Danilo"
 i =34
 a =2023
-print(f"olá {n} você tem {i}anos e estamos no ano {a}")
+m="Maria"
+print(f"olá {n} você tem {i}anos e estamos no ano {a} e você é amigo da {m}")
