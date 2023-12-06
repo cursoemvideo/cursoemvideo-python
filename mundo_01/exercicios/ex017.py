273202ea4537e0e56aa54a8998cc44a68c31c1cc
Vitor Brandão <vitor.bjd20@gmail.com>
2023-12-05 21:00:36 -0300

    ultimos ajustes

--- mundo_01/exercicios/ex017.py
+++
@@ -1,5 +0,0 @@
-from math import hypot
-co = float(input('Comprimento do cateto oposto: '))
-ca = float(input('Comprimento do cateto adjacente: '))
-hi = hypot(co, ca)
-print('A hipotenusa vai medir {:.2f}'.format(hi))
